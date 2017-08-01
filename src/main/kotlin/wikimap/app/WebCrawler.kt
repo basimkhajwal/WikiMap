1354f233921b23c57117ee5805fd0d3fa6d50729
Jamie Stirling <jamiestirling2000@gmail.com>
2017-08-01 22:35:06 +0100

    Finished implementing WebCrawler

--- src/main/kotlin/wikimap/app/WebCrawler.kt
+++ src/main/kotlin/wikimap/app/WebCrawler.kt
@@ -1,17 +1,11 @@
 package wikimap.app
 
 import java.net.URL
+import javax.xml.ws.WebEndpoint
 
-class WebCrawler(seedUrl:String){
-    var seedUrl:String? = null
-
+class WebCrawler(val seedUrl:String){
     val maxDepth = 2
     val crawledUrls:MutableList<String> = mutableListOf()
-
-    init{
-        this.seedUrl = seedUrl
-        println(downloadPage(seedUrl))
-    }
 
     fun crawl(currentUrl:String, depth:Int){
         if(depth > maxDepth || crawledUrls.contains(currentUrl)) {
@@ -20,16 +14,72 @@ class WebCrawler(seedUrl:String){
             crawledUrls.add(currentUrl)
             println(currentUrl + " " + depth)
             val rawPage = downloadPage(currentUrl)
+            val intro = extractIntro(rawPage)
+
+            val introLinks = extractLinks(intro)
+
+            for (link in introLinks){
+                if (validateLink(link)){
+                    if (linkIsComplete(link)){
+                        crawl(link, depth + 1)
+                    }
+                    else{
+                        crawl(seedUrl + link, depth + 1)
+                    }
+                }
+            }
+
         }
     }
 
     fun downloadPage(urlString:String):String{
-        //val url = URL(urlString)
-        //return url.readText()
-        return ""
+        val url = URL(urlString)
+        return url.readText()
     }
-}
 
-fun main(args: Array<String>) {
-    var test = wikimap.app.WebCrawler("https://en.wikipedia.org/wiki/Main_Page")
+    fun extractIntro(rawPage:String):String{
+        val introStartIndex = rawPage.indexOf("<p>")
+        var introStopIndex = rawPage.indexOf("<div id=\"toctitle\"", introStartIndex + 1)
+
+        if (introStopIndex == -1){
+            introStopIndex = rawPage.indexOf("</p>", introStartIndex + 1)
+        }
+
+        val rawIntro = rawPage.slice(IntRange(introStartIndex, introStopIndex - 1))
+        return rawIntro
+    }
+
+    fun extractLinks(str:String):MutableList<String>{
+        var searchIndex = 0
+
+        val linkList = mutableListOf<String>()
+
+        while (true){
+            val startLink = str.indexOf("<a href", searchIndex)
+
+            if (startLink == -1)
+                break
+            else{
+                val startQuote = str.indexOf("\"", startLink + 1)
+                val endQuote = str.indexOf("\"", startQuote + 1)
+
+                linkList.add(str.substring(startQuote + 1, endQuote))
+                searchIndex = endQuote
+            }
+        }
+
+        return linkList
+    }
+
+    fun linkIsComplete(link:String):Boolean{
+        return link.contains(seedUrl)
+    }
+
+    fun validateLink(link:String):Boolean{
+        if (link.contains("#")){
+            return false
+        }else{
+            return (!link.contains("https://") || link.contains(seedUrl))
+        }
+    }
 }
