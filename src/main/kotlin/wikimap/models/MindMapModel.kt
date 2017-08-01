9415ad33bd5e5be5ea6b8420c68d9ea7086aa019
Basim Khajwal <basimkhajwal@gmail.com>
2017-08-01 14:03:32 +0100

    Added mind map model

---
+++ src/main/kotlin/wikimap/models/MindMapModel.kt
@@ -0,0 +1,18 @@
+package wikimap.models
+
+/**
+ * Created by Basim on 01/08/2017.
+ */
+data class MindMapModel(
+    var root: MindMapNode
+)
+
+/**
+ * A single element of a MindMap
+ */
+data class MindMapNode(
+    var key: String,
+    var x: Int, var y: Int, /* Bottom-left grid coords */
+    var width: Int, var height: Int,
+    val children: MutableList<MindMapNode>
+)
