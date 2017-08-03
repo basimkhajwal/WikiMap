9fd75c9e650ce4eef6e596ff6291b7b862d1806a
Basim Khajwal <basimkhajwal@gmail.com>
2017-08-03 13:21:30 +0100

    Moved connection onto another line

---
+++ src/main/kotlin/wikimap/view/NodeConnection.kt
@@ -0,0 +1,42 @@
+package wikimap.view;
+
+import javafx.scene.layout.Pane
+import javafx.scene.shape.Line
+import javafx.scene.shape.Rectangle
+import javafx.scene.shape.Shape
+import tornadofx.*
+
+class NodeConnection(val parent: NodeViewModel, val child: NodeViewModel) {
+        val line = Line(0.0,0.0,0.0,0.0)
+        val clipPane = Pane(line)
+
+        fun refresh() {
+            line.startX = parent.node.layoutX + parent.node.prefWidth / 2
+            line.startY = parent.node.layoutY + parent.node.prefHeight / 2
+            line.endX = child.node.layoutX + child.node.prefWidth / 2
+            line.endY = child.node.layoutY + child.node.prefHeight / 2
+
+            val total = Rectangle(clipPane.layoutBounds.width, clipPane.layoutBounds.height)
+            val mask = Shape.subtract(total, Shape.union(parent.rect, child.rect))
+            clipPane.clip = mask
+        }
+
+        fun show() {
+            clipPane.show()
+            refresh()
+        }
+
+        fun hide() {
+            clipPane.hide()
+        }
+
+        fun close() {
+            clipPane.removeFromParent()
+        }
+
+        init {
+            child.parent = this
+            parent.main.buttonPane += clipPane
+            show()
+        }
+    }
