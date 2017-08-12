1a17c7baa4706fbb83688df77ef8f3f56dc8b162
Basim Khajwal <basimkhajwal@gmail.com>
2017-08-12 14:11:59 +0100

    Revert "Revert "Renamed package""
    This reverts commit 426591a5c7adfa4ed9f3a2d68b1eb1e3633907aa.

---
+++ src/main/kotlin/wikimap/views/GridView.kt
@@ -0,0 +1,54 @@
+package wikimap.views
+
+import javafx.scene.canvas.Canvas
+import javafx.scene.layout.Pane
+import javafx.scene.paint.Color
+import tornadofx.*
+
+/**
+ * Created by Basim on 04/08/2017.
+ */
+class GridView : View() {
+
+    val canvas = Canvas(800.0, 600.0)
+    val spacing = 20
+
+    override val root = Pane(canvas)
+
+    init {
+        root.layoutBoundsProperty().onChange { draw() }
+    }
+
+    private fun draw() {
+
+        val width = root.layoutBounds.width
+        val height = root.layoutBounds.height
+        canvas.width = width
+        canvas.height = height
+
+        val g2d = canvas.graphicsContext2D
+        val hw = width/2
+        val hh = height/2
+        val xRange = Math.floor(width / spacing).toInt() + 1
+        val yRange = Math.floor(height / spacing).toInt() + 1
+
+        g2d.clearRect(0.0, 0.0, width, height)
+        g2d.stroke = Color.LIGHTGRAY
+        g2d.lineWidth = 0.5
+
+        for (x in (-xRange/2)..(xRange/2)) {
+            g2d.strokeLine(x*spacing + hw, 0.0, x*spacing + hw, height)
+        }
+        for (y in (-yRange/2)..(yRange/2)) {
+            g2d.strokeLine(0.0, y*spacing + hh, width, y*spacing + hh)
+        }
+    }
+
+    fun fromGridCoords(x: Double, y: Double): Pair<Double, Double> {
+        return Pair(x*spacing + canvas.width/2, y*spacing+ canvas.height/2)
+    }
+
+    fun toGridCoords(x: Double, y: Double): Pair<Double, Double> {
+        return Pair((x - canvas.width/2) / spacing, (y - canvas.height/2) / spacing)
+    }
+}
---
+++ src/main/kotlin/wikimap/views/MenuBarView.kt
@@ -0,0 +1,62 @@
+package wikimap.views
+
+import javafx.scene.control.MenuBar
+import javafx.stage.FileChooser
+import tornadofx.*
+import wikimap.models.MindMap
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.StandardOpenOption
+
+/**
+ * Created by Basim on 09/08/2017.
+ */
+class MenuBarView : View() {
+
+    private var file: File? = null
+
+    val main: MainView by param()
+
+    override val root = MenuBar()
+
+    init {
+        with(root) {
+            menu("File") {
+                item("Open") {
+                    action {
+                        file = FileChooser().showOpenDialog(currentWindow)
+                        loadFromFile()
+                    }
+                }
+
+                item("Save") {
+                    action {
+                        if (file == null) {
+                            file = FileChooser().showSaveDialog(currentWindow)
+                        }
+                        saveToFile()
+                    }
+                }
+
+                item("Save As") {
+                    action {
+                        file = FileChooser().showSaveDialog(currentWindow)
+                        saveToFile()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun loadFromFile() {
+        val fileContents = String(Files.readAllBytes(file!!.toPath()))
+        val model = MindMap.deserialize(fileContents)
+
+        main.loadModel(model)
+    }
+
+    private fun saveToFile() {
+        val fileData = main.mindMap.serialize().toByteArray()
+        Files.write(file!!.toPath(), fileData, StandardOpenOption.CREATE_NEW)
+    }
+}
