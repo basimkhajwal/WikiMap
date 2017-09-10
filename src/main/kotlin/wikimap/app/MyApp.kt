cf2994453b612c2a2896d85de64bfcb97dc8bcc2
Basim Khajwal <basimkhajwal@gmail.com>
2017-09-10 22:30:08 +0100

    Updated build settings

--- src/main/kotlin/wikimap/app/MyApp.kt
+++ src/main/kotlin/wikimap/app/MyApp.kt
@@ -1,6 +1,11 @@
 package wikimap.app
 
+import javafx.stage.Stage
 import tornadofx.App
 import wikimap.views.MainView
 
 class MyApp: App(MainView::class)
+
+fun main(args: Array<String>) {
+    MyApp().start(Stage())
+}
